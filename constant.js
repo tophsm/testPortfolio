4d1215ff5a2642067dd63cb51de44628abdddd17
tophsm <67989403+tophsm@users.noreply.github.com>
2025-03-26 19:29:13 -0700

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,13 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my computer. Here is my <a href= "https://github.com/tophsm" target="blank">Github</a>!`,
+    "cs-degree": `This is my resume. I hang it on the wall because I've worked very hard and I am proud of it.`,
+    "sofa-table": `This is my couch. I like to read here. I am currently reading Empire of the Damned.`,
+    tv: `This is my tv. I like watching tv and movies. The last movie I saw was Wicked.`,
+    bed: `This is where I try to get sleep. Usually, I watch tv, read, or doomscroll.`,
+    resume: `This is my desk. I have pictures of my dog on it.`,
+    projects: 'This is my first project using the Kaboom.js library. I am proud.',
+    library: `I have many books!`,
+    exit: `To exit Sandy's portfolio, close the tab.`
+}
